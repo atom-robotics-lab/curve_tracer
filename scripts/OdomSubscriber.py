51d658f5fa1e51eefbba745813cc9c9c456f149b
Jasmeet Singh <jasmeet0915@gmail.com>
2021-12-27 19:59:37 +0530

    Tested & Refactored scripts

--- scripts/OdomSubscriber.py
+++ scripts/OdomSubscriber.py
@@ -17,16 +17,20 @@ class OdomSubscriber():
       
      def get_position(self):
          odom_position = {"x" :self.odom_data.pose.pose.position.x, "y" :self.odom_data.pose.pose.position.y, "z" :self.odom_data.pose.pose.position.z}
-         return (Odom_position)
+         return (odom_position)
          
      def get_orientation(self):
          odom_orientation = {"w" :self.odom_data.pose.pose.orientation.w, "x" :self.odom_data.pose.pose.orientation.x , "y" :self.odom_data.pose.pose.orientation.y, "z" : self.odom_data.pose.pose.orientation.z}
          return (odom_orientation)
          
 
-
 call_odomsubs = OdomSubscriber()
-while(True):
-  print(call_odomsubs.get_orientation()['x'])
-rospy.spin()
 
+rate = rospy.Rate(10)
+
+while(True):
+    print("Odom Position:")
+    print(call_odomsubs.get_position())
+    print("Odom orientation:")
+    print(call_odomsubs.get_orientation())
+    rate.sleep()
--- scripts/WaypointManager.py
+++ scripts/WaypointManager.py
@@ -2,15 +2,14 @@
 
 import numpy as np
 
-class Waypoint_Mananger:
 
+class WaypointMananger:
   def __init__(self,res=1):
     self.index = 0
     self.res = res
     self.way_point()
 
   def way_point(self):
-
     #create a numpy array of 'res' points in range 0 to 2*pi
     x = np.linspace(0, 2*np.pi, num = self.res, endpoint=True)[1:]
     #create a numpy array of given path function 
@@ -25,9 +24,15 @@ class Waypoint_Mananger:
     #Checking if the bot has reached the destination
     if self.index >= len(self.list_waypoints):
       print("Bot has reached its destination")
-      return None 
+      return 
     
     self.next_waypoint = self.list_waypoints[self.index]
     self.index = self.index + 1
   
     return self.next_waypoint
+
+
+if __name__ == "__main__":
+  waypoint_manager = WaypointMananger(res=10)
+  print(waypoint_manager.way_point())
+  print(waypoint_manager.get_next_waypoint())
